8daad3c8c56bef91bdcadd89792022fc3ea82c73
zigdanis <zigdanis@gmail.com>
2018-12-14 18:55:18 +0300

    Initial Commit

---
+++ Aging/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aging
+//
+//  Created by zigdanis on 14/12/2018.
+//  Copyright © 2018 zigdanis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
