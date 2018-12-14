131904e71b82cce1b476351ece2b073919df4580
zigdanis <zigdanis@gmail.com>
2018-12-15 00:42:57 +0300

    Add Icon, calculate and show Aging on the main screen

--- Aging/Classes/ViewController.swift
+++ Aging/Classes/ViewController.swift
@@ -9,10 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var ageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        maintainCurrentAge()
     }
 
+    private func maintainCurrentAge() {
+        setupCurrentAge()
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] _ in
+            self?.setupCurrentAge()
+        }
+    }
+    
+    private func setupCurrentAge() {
+        
+        let initial = DateComponents(year: 2018, month: 10, day: 20, hour: 13, minute: 25, second: 0)
+        let now = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: Date())
+        let comps = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: initial, to: now)
+        let years = comps.year ?? 0
+        let months = comps.month ?? 0
+        let days = comps.day ?? 0
+        let hours = comps.hour ?? 0
+        let minutes = comps.minute ?? 0
+        let seconds = comps.second ?? 0
+        
+        var result = ""
+        if years > 0 {
+            result += " " + String(format: NSLocalizedString("%li years", comment: "Years"), years)
+        }
+        if months > 0 {
+            result += " " + String(format: NSLocalizedString("%li months", comment: "Months"), months)
+        }
+        if days > 0 {
+            result += " " + String(format: NSLocalizedString("%li days", comment: "Days"), days)
+        }
+        if hours > 0 {
+            result += " " + String(format: NSLocalizedString("%li hours", comment: "Hour"), hours)
+        }
+        if minutes > 0 {
+            result += " " + String(format: NSLocalizedString("%li minutes", comment: "Minutes"), minutes)
+        }
+        if seconds > 0 {
+            result += " " + String(format: NSLocalizedString("%li seconds", comment: "Seconds"), seconds)
+        }
+        ageLabel.text = result.trimmingCharacters(in: .whitespaces)
+    }
+    
+    
 }
 
