f0e281388acdafa7a30592f545a92276ac2f34f3
zigdanis <zigdanis@gmail.com>
2019-02-24 23:51:59 +0300

    Remove UIButton extension for vertical alignment

--- Aging/Views/VerticalButton.swift
+++ Aging/Views/VerticalButton.swift
@@ -33,4 +33,29 @@ class VerticalButton: UIButton {
     override var intrinsicContentSize: CGSize {
         get { return verticalAlignedIntrinsicContentSize() }
     }
+    
+    private func centerVertically(padding: CGFloat = 6) {
+        let imageSize = imageView?.frame.size ?? .zero
+        let titleSize = titleLabel?.frame.size ?? .zero
+        
+        let totalHeight = imageSize.height + titleSize.height + padding
+        let imageTop = -(totalHeight - imageSize.height)
+        imageEdgeInsets = UIEdgeInsets(top: imageTop, left: 0, bottom: 0, right: -titleSize.width)
+        
+        let titleBottom = -(totalHeight - titleSize.height)
+        titleEdgeInsets = UIEdgeInsets(top: 0, left: -imageSize.width, bottom: titleBottom, right: 0)
+    }
+    
+    private func verticalAlignedIntrinsicContentSize(padding: CGFloat = 6) -> CGSize {
+        let maxSize = CGSize(width: CGFloat.greatestFiniteMagnitude, height: CGFloat.greatestFiniteMagnitude)
+        
+        if let titleSize = titleLabel?.sizeThatFits(maxSize), let imageSize = imageView?.sizeThatFits(maxSize) {
+            let width = ceil(max(imageSize.width, titleSize.width))
+            let height = ceil(imageSize.height + titleSize.height + padding)
+            
+            return CGSize(width: width, height: height)
+        }
+        
+        return super.intrinsicContentSize
+    }
 }
