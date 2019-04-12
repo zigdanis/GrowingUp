e7a3cffa516cfce5f59b36781c7903ac2e4b930e
Danis Ziganshin <zigdanis@gmail.com>
2019-04-12 18:01:39 +0300

    Add Tests for EmptyPersonPresenter

---
+++ GrowingUpTests/Helpers/Routers/EmptyPersonViewRouterSpy.swift
@@ -0,0 +1,19 @@
+//
+//  EmptyPersonViewRouterSpy.swift
+//  GrowingUpTests
+//
+//  Created by zigdanis on 11/04/2019.
+//  Copyright © 2019 zigdanis. All rights reserved.
+//
+
+import Foundation
+@testable import GrowingUp
+
+final class EmptyPersonViewRouterSpy: EmptyPersonViewRouter {
+
+	var didCallPresentAddPerson = false
+
+	func presentAddPerson(addPersonPresenterDelegate: AddPersonPresenterDelegate?) {
+		didCallPresentAddPerson = true
+	}
+}
---
+++ GrowingUpTests/Presenters/EmptyPersonPresenterTests.swift
@@ -0,0 +1,29 @@
+//
+//  EmptyPersonPresenterTests.swift
+//  GrowingUpTests
+//
+//  Created by zigdanis on 11/04/2019.
+//  Copyright © 2019 zigdanis. All rights reserved.
+//
+
+import XCTest
+@testable import GrowingUp
+
+class EmptyPersonPresenterTests: XCTestCase {
+
+	var sut: EmptyPersonPresenterImplementation!
+	let routerSpy = EmptyPersonViewRouterSpy()
+	let delegateSpy = AddPersonPresenterDelegateSpy()
+
+    override func setUp() {
+    	sut = EmptyPersonPresenterImplementation(router: routerSpy, addPersonPresenterDelegate: delegateSpy)
+    }
+
+	func test_SUT_WhenCalledAddButtonPressed_ShouldCallRouter() {
+		// When
+		sut.addButtonPressed()
+		// Then
+		XCTAssertTrue(routerSpy.didCallPresentAddPerson, "Expected to receive a call in Router")
+	}
+
+}
