3e12a7754e79efa701bdf0630d4ab2e634f80ebe
Danis Ziganshin <zigdanis@gmail.com>
2019-09-26 17:34:41 +0300

    Remove Mixpanel and SwiftyBeaver Analytics libraries from the project because they are dumb. Meanwhile Looking for alternatives

--- Core/Logging.swift
+++ Core/Logging.swift
@@ -7,44 +7,36 @@
 //
 
 import Foundation
-import SwiftyBeaver
-import Mixpanel
 
 public enum Logging {
 
 	public static func setup() {
-		let console = ConsoleDestination()
-		let file = FileDestination()
-		let obfuscator = Obfuscator()
-		let appId = obfuscator.reveal(key: Constants.obfusSBAppId)
-		let appSecretId = obfuscator.reveal(key: Constants.obfusSBAppSecret)
-		let encryption = obfuscator.reveal(key: Constants.obfusSBEncryption)
-		let platform = SBPlatformDestination(appID: appId, appSecret: appSecretId, encryptionKey: encryption)
-		#if DEBUG
-		file.logFileURL = URL(fileURLWithPath: "/tmp/swiftybeaver.log")
-		#endif
-		SwiftyBeaver.addDestination(console)
-		SwiftyBeaver.addDestination(file)
-		SwiftyBeaver.addDestination(platform)
-
-		Mixpanel.sharedInstance(withToken: obfuscator.reveal(key: Constants.obfusMPToken))
+//		let console = ConsoleDestination()
+//		let file = FileDestination()
+//		let obfuscator = Obfuscator()
+//		let appId = obfuscator.reveal(key: Constants.obfusSBAppId)
+//		let appSecretId = obfuscator.reveal(key: Constants.obfusSBAppSecret)
+//		let encryption = obfuscator.reveal(key: Constants.obfusSBEncryption)
+//		let platform = SBPlatformDestination(appID: appId, appSecret: appSecretId, encryptionKey: encryption)
+//		#if DEBUG
+//		file.logFileURL = URL(fileURLWithPath: "/tmp/swiftybeaver.log")
+//		#endif
+//		SwiftyBeaver.addDestination(console)
+//		SwiftyBeaver.addDestination(file)
+//		SwiftyBeaver.addDestination(platform)
+//
+//		Mixpanel.sharedInstance(withToken: obfuscator.reveal(key: Constants.obfusMPToken))
 	}
 
 	public static func logError(_ error: CoreError) {
 		let value = "❌ \(error.title)\n\(error.message)"
-		SwiftyBeaver.error(value)
-		Mixpanel.sharedInstance()?.track(value)
 	}
 
 	public static func logMessage(_ message: String, params: [String: String]? = nil) {
 		let value = "✍️ \(message)"
-		SwiftyBeaver.debug(value)
-		Mixpanel.sharedInstance()?.track(value, properties: params)
 	}
 
 	public static func logWarning(_ warning: String) {
 		let value = "⚠️ \(warning)"
-		SwiftyBeaver.warning(value)
-		Mixpanel.sharedInstance()?.track(value)
 	}
 }
