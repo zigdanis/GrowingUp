13f7c064e1db4a8cc825f6bcb8ec95df60b2caee
Danis Ziganshin <zigdanis@gmail.com>
2026-07-27 23:31:23 +0300

    Test malformed constructor formatting

--- Core/Entities/PersonImage.swift
+++ Core/Entities/PersonImage.swift
@@ -17,7 +17,10 @@ public struct PersonImage: Equatable {
 	}
 
 	public init(id: UUID? = nil, uiImage: UIImage? = nil) {
-		self.id = id ?? UUID()
+		self.id =
+			id
+			?? UUID()
+
 		self.uiImage = uiImage
 	}
 
