52e0329eb95f997e155dc5cb4b30ac6444ed6684
Danis Ziganshin <zigdanis@gmail.com>
2026-09-19 20:50:21 +0300

    Dismiss the calendar through the name field before saving

--- GrowingUpUITests/JourneyTests.swift
+++ GrowingUpUITests/JourneyTests.swift
@@ -160,7 +160,7 @@ final class JourneyTests: XCTestCase {
 		let date = app.datePickers["editor.birthday"]
 		date.buttons.firstMatch.tap()
 		app.buttons["Sunday, January 10"].tap()
-		app.tap()
+		name.tap()
 		app.buttons["editor.save"].tap()
 		XCTAssertTrue(app.buttons["person.edit"].waitForExistence(timeout: 5))
 		XCTAssertEqual(app.staticTexts["person.name"].label, "Ada")
